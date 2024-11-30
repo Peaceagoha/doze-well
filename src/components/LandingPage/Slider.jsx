f11eb7c48d9894940297906813549466f1a28491
Yuwa <iamnosakare@gmail.com>
2024-11-30 13:39:44 +0100

    new styles added

--- src/components/LandingPage/Slider.jsx
+++ src/components/LandingPage/Slider.jsx
@@ -2,21 +2,46 @@ import PropTypes from "prop-types";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { responsive } from "../../data/data";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa"; // Import icons
 
 const Slider = ({ slides }) => {
+  // Custom Left Arrow
+  const CustomLeftArrow = ({ onClick }) => (
+    <button
+      onClick={onClick}
+      className="arrow absolute -left-0 top-1/2 transform -translate-y-1/2 bg-purple-600 text-white p-2 rounded-full shadow-md  z-10"
+    >
+      <FaChevronLeft />
+    </button>
+  );
+
+  // Custom Right Arrow
+  const CustomRightArrow = ({ onClick }) => (
+    <button
+      onClick={onClick}
+      className="arrow absolute right-0 top-1/2 transform -translate-y-1/2  text-white p-2 rounded-full shadow-md  z-10"
+    >
+      <FaChevronRight />
+    </button>
+  );
+
   return (
-    <section className="py-10">
-      <Carousel responsive={responsive}>
+    <section className="second__slider py-10 relative">
+      <Carousel
+        responsive={responsive}
+        customLeftArrow={<CustomLeftArrow />}
+        customRightArrow={<CustomRightArrow />}
+      >
         {slides.map((slide, i) => (
           <div
             key={i}
-            className="bg-purple-dark-dozewell text-white flex flex-col gap-8 items-start px-4 py-4 mr-4 h-full"
+            className="card__text bg-purple-dark-dozewell text-white flex flex-col gap-8 items-start px-4 py-12 mr-4 h-full"
           >
             <h1 className="font-bold">{slide.title}</h1>
             <p>{slide.body}</p>
-            <button className="bg-purple-dozewell text-white px-3 py-2 rounded-md">
-              Learn more ..
-            </button>{" "}
+            <button className="button__card bg-purple-dozewell text-white px-4 py-3 rounded-md">
+              Learn more...
+            </button>
           </div>
         ))}
       </Carousel>
